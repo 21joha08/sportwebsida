ab45fbbcab56171889631c7cdef6515747556ffc
willberj <wib@live.se>
2023-10-16 12:11:40 +0200

    fixade positionering

--- src/Pages/Allsvenskan.js
+++ src/Pages/Allsvenskan.js
@@ -9,10 +9,10 @@ function Allsvenskan() {
   const [events] = useFetch("events", "&limit=10");
 
   return (
-    <div className="container-fluid">
+    <div className="container">
       <div className="row">
         <div className="col-2">
-          <div className="image-container border border-primary border-3">
+          <div className="image-container">
             {logos.map((team, index) => (
               <div key={team.id} className="logo-list text-center">
                 <img src={team.logos.small} alt={team.name} />
@@ -20,9 +20,9 @@ function Allsvenskan() {
             ))}
           </div>
         </div>
-        <div className="col-10">
+        <div className="col-10 text-center">
           <h1>Allsvenskan Teams</h1>
-          <div className="row event-container border border-warning border-3">
+          <div className="row event-container">
             {events.map((event) => (
               <MatchBox key={event.id} event={event} />
             ))}
